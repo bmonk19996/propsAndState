f6f717cd9fcc5a3e837221bfebf8f0b3122cce8f
MonkComputer <bmonk1996@yahoo.com>
2023-02-13 14:00:30 -0600

    border added on select

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 // write your Color component here
 const Picker = () => {
-const [selectedColor,setSelectedColor] = useState('')
+  const [selectedColor, setSelectedColor] = useState("");
 
-const Color = (props) => {
-
-  function ifSelected(){
-    if(props.color===selectedColor){
-      return true
-    } 
-    else{
-      return false
+  const Color = (props) => {
+    function ifSelected() {
+      if (props.color === selectedColor) {
+        console.log(selectedColor);
+        return true;
+      } else {
+        return false;
+      }
     }
+
+    return (
+      <div
+        className={ifSelected() ? props.color + " selected" : props.color}
+        onClick={() => props.changeColor(props.color)}
+      />
+    );
+  };
+
+  function changeColor(color) {
+    setSelectedColor(color);
   }
-  
-  return (
-    <div className={props.color} onClick={() =>props.changeColor(props.color)}/>
-  )
-}
-
-function changeColor(color) {
-  setSelectedColor(color)
-}
-
-
 
   return (
     <div id="container">
@@ -34,15 +34,12 @@ function changeColor(color) {
         <div className={selectedColor}>{selectedColor}</div>
       </div>
       <div id="colors-list">
-      <Color color='red' changeColor={changeColor}/>
-      <Color color='blue'changeColor={changeColor}/>
-      <Color color='black'changeColor={changeColor}/>
+        <Color color="red" changeColor={changeColor} />
+        <Color color="blue" changeColor={changeColor} />
+        <Color color="black" changeColor={changeColor} />
       </div>
     </div>
   );
-}
+};
 
-ReactDOM.render(
-  <Picker />,
-  document.getElementById('app')
-);
+ReactDOM.render(<Picker />, document.getElementById("app"));
